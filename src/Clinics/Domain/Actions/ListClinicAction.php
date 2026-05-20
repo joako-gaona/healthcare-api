70b4c9b363f4b06762f16c81c9ecc90a3add14f4
joako-gaona <joaquin.gaona@lightit.io>
2026-05-20 17:05:26 -0300

    fix: add orderByDesc

--- src/Clinics/Domain/Actions/ListClinicAction.php
+++ src/Clinics/Domain/Actions/ListClinicAction.php
@@ -18,7 +18,7 @@ class ListClinicAction
         return QueryBuilder::for(Clinic::class)
             ->allowedFilters(['name', 'address'])
             ->allowedSorts(['id', 'name', 'address'])
-            ->orderBy('id', 'desc')
+            ->orderByDesc('id')
             ->paginate();
     }
 }
