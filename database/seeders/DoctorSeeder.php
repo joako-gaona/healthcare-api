f13febdaf50378dfc91fa091fe10b9f3b98af35c
joako-gaona <joaquin.gaona@lightit.io>
2026-05-26 16:44:46 -0300

    fix: add hasAttached to doctor seeder

--- database/seeders/DoctorSeeder.php
+++ database/seeders/DoctorSeeder.php
@@ -7,23 +7,21 @@ namespace Database\Seeders;
 use Database\Factories\DoctorFactory;
 use Illuminate\Database\Seeder;
 use Lightit\Clinics\Domain\Models\Clinic;
-use Lightit\Doctors\Domain\Models\Doctor;
 
 class DoctorSeeder extends Seeder
 {
     public function run(): void
     {
-        DoctorFactory::new()
-            ->count(10)
-            ->create()
-            ->each(static function (Doctor $doctor): void {
-                $clinicIds = Clinic::query()
-                    ->inRandomOrder()
-                    ->take(random_int(1, 3))
-                    ->pluck('id')
-                    ->all();
-
-                $doctor->clinics()->sync($clinicIds);
-            });
+        for ($i = 0; $i < 10; $i++) {
+            DoctorFactory::new()
+                ->hasAttached(
+                    Clinic::query()
+                        ->inRandomOrder()
+                        ->take(random_int(1, 3))
+                        ->get(),
+                    relationship: 'clinics',
+                )
+                ->create();
+        }
     }
 }
